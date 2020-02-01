e1ebfaeef1655094eb88cc556d83ff29cb035255
Gajus Kuizinas <gajus@gajus.com>
2020-02-01 12:43:12 +0900

    test: add a todo

--- test/extract-date/extractDate/edge-cases.js
+++ test/extract-date/extractDate/edge-cases.js
@@ -10,31 +10,31 @@ import {
 } from 'date-fns';
 import extractDate from '../../../src/extractDate';
 
-let clock;
-
-beforeEach(() => {
-  clock = sinon.useFakeTimers();
+beforeEach((t) => {
+  t.context.clock = sinon.useFakeTimers();
 });
 
-afterEach(() => {
-  clock.restore();
+afterEach((t) => {
+  t.context.clock.restore();
 });
 
 test('parses ISO 8601 date-time format', (t) => {
-  clock.tick(parseDate('2000-01-02', 'yyyy-MM-dd', new Date()).getTime());
+  t.context.clock.tick(parseDate('2000-01-02', 'yyyy-MM-dd', new Date()).getTime());
 
   t.deepEqual(extractDate('2000-01-02T00:00'), [{date: '2000-01-02'}]);
 });
 
 test('extracts date matching multiple formats once', (t) => {
-  clock.tick(parseDate('2000-01-02', 'yyyy-MM-dd', new Date()).getTime());
+  t.context.clock.tick(parseDate('2000-01-02', 'yyyy-MM-dd', new Date()).getTime());
 
   // 'foo bar' prefix is required to ensure that substring matching is correctly advancing.
   t.deepEqual(extractDate('foo bar 02/01/2000', {direction: 'DMY'}), [{date: '2000-01-02'}]);
 });
 
 test('full-year formats are used regardless of whether direction defines Y', (t) => {
-  clock.tick(parseDate('2000-01-02', 'yyyy-MM-dd', new Date()).getTime());
+  t.context.clock.tick(parseDate('2000-01-02', 'yyyy-MM-dd', new Date()).getTime());
 
   t.deepEqual(extractDate('02/01/2020', {direction: 'DM'}), [{date: '2020-01-02'}]);
 });
+
+test.todo('interprets 24:00 time as the next date');
